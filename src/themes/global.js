ed0a4b72bea260fb7ff9d77a7829fe542b345ea6
Pillippa Perez <ignacia.pp@gmail.com>
2019-10-05 17:43:48 -0300

    add themes

---
+++ src/themes/global.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+  }
+  *, *::after, *::before {
+    box-sizing: border-box;
+  }
+  body {
+    align-items: center;
+    background: ${({ theme }) => theme.primaryDark};
+    color: ${({ theme }) => theme.primaryLight};
+    display: flex;
+    width: 100wv;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    height: 100vh;
+    justify-content: center;
+    text-rendering: optimizeLegibility;
+    #root {
+      width: 100%;
+        > section {
+        width: 80%;
+        display: flex;
+        justify-content: center;
+      }
+    }
+  }`;
