d7b5ce7879aaefd9021f01dc9f9f3e80b36082c3
Pillippa Perez <ignacia.pp@gmail.com>
2019-10-05 17:44:22 -0300

    container and apply redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
+import { GlobalStyles } from "./themes/global";
+import { theme } from "./themes/default";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from "./store";
+import App from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ThemeProvider>,
+  document.getElementById("root")
+);
